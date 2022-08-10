1231a4a60ad7f9d74b3fb459c8a47265853e4e25
WkRafal <wiezikrafal@gmail.com>
2022-08-11 00:50:20 +0200

    first look

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Random random = new Random();
+        int rzutKomp;
+        int gracz;
+
+        do {
+            rzutKomp = random.nextInt(6) + 1;
+            System.out.println("komputer wylosowal " + rzutKomp);
+            gracz = random.nextInt(6) + 1;
+            System.out.println("gracz wylosowal " + gracz);
+
+            if (rzutKomp != gracz)
+                System.out.println("przegrałes");;
+
+        }while (rzutKomp != gracz);
+
+        System.out.println("wygrałeś");
     }
 }
